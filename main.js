b2453226385c339bf87cfbddfd0f16170da8bbc4
Alyssa <alyssaw90@gmail.com>
2014-11-24 09:05:59 -0800

    Code for winning combinations, but it is not working

--- main.js
+++ main.js
@@ -4,53 +4,65 @@ window.onload = function () {
     var counter = 1;
     //Declearing Square
     var square = document.getElementsByClassName("square");
-    //Declearing Clear
+    //Rest button
     var clear = document.getElementById("end");
-    //For displaying whos turn it is
+    //Who's turn is it?
     var turn = document.getElementById("turn");
-    //Storing user's combinations
+    //Array for user combinations
     var userCombinations = [];
     //The winning combinations
     var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
+      //Winner
+      var winner = function() {
+      	for (var j =0; j < winnerCombinations.length; j++){
+      		if ((squares[winnerCombinations[j][0]].innerText !== "--") && (squares[winnerCombinations[j][1]].innerText === squares[winnerCombinations [j][1]].innerText) && (squares[winnerCombinations[j][2]].innerText === squares[winnerCombinations[j][0].innerText])) {
+      			alert(squares[winnerCombinations[j][1]].innerText + " wins!");
+      			return squares[winnerCombinations[j][1]].innerText;
+      		}
+      	}
+      }
+
+
 
     //Clicking the squares  
     var clickSquare = function(event) {
-        event.onclick = function () {
-            if (event.innerHTML === ""){ 
-                if (counter % 2 === 0) {
-                    event.style.color = "blue";
-                    event.style.fontFamily = "sans-serif";
-                    event.innerHTML = "O";          
-                    turn.innerHTML = "It is X's Move";
-                    turn.style.fontSize="25px";     
-                    counter ++;
-                } else {
-                    event.style.color = "red";
-                    event.style.fontFamily = "sans-serif";
-                    event.innerHTML = "X";  
-                    turn.innerHTML = "It is O's Move";
-                    turn.style.fontSize="25px";
-                    counter ++;
-                }
-            } else {
-                alert("This move is already taken!");
-            }
-        }
+    	event.onclick = function () {
+    		if (event.innerHTML === ""){ 
+    			if (counter % 2 === 0) {
+    				event.style.color = "blue";
+    				event.style.fontFamily = "sans-serif";
+    				event.innerHTML = "O";          
+    				turn.innerHTML = "X's Move";
+    				turn.style.fontSize="25px";     
+    				counter ++;
+    			} else {
+    				event.style.color = "red";
+    				event.style.fontFamily = "sans-serif";
+    				event.innerHTML = "X";  
+    				turn.innerHTML = "O's Move";
+    				turn.style.fontSize="25px";
+    				counter ++;
+    			}
+    		} else {
+    			alert("This move is already taken!");
+    		}
+    	}
     }
 
     for(var i=0; i<9; i++) {
-        clickSquare(square[i]);
+    	clickSquare(square[i]);
     }   
 
 
-    //Doing a loop in the Clear function
+
+    //Reset button
     clear.onclick = function () {
-        for(var i = 0; i <9; i++) {
-            square[i].innerHTML = "" ;
-            turn.innerHTML = "X's Move";
-            counter = 1;
-            console.log("counter:",counter)
-        }
+    	for(var i = 0; i <9; i++) {
+    		square[i].innerHTML = "" ;
+    		turn.innerHTML = "X's Move";
+    		counter = 1;
+    		console.log("counter:",counter)
+    	}
     }	
 }
